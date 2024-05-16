2deccde71bbb0c37205bff2130ee2d018a16b4df
aaron-soto <aaron.m.soto1@gmail.com>
2024-05-16 09:23:58 -0700

    refactor: Handle errors when getting access token and currently playing track in Spotify API

--- api/spotify.js
+++ api/spotify.js
@@ -9,24 +9,32 @@ module.exports = async (req, res) => {
     const tokenUrl = 'https://accounts.spotify.com/api/token';
     const data = qs.stringify({ grant_type: 'client_credentials' });
 
-    const response = await axios.post(tokenUrl, data, {
-      headers: {
-        'Authorization': 'Basic ' + Buffer.from(clientId + ':' + clientSecret).toString('base64'),
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-    });
-
-    return response.data.access_token;
+    try {
+      const response = await axios.post(tokenUrl, data, {
+        headers: {
+          'Authorization': 'Basic ' + Buffer.from(clientId + ':' + clientSecret).toString('base64'),
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      });
+      return response.data.access_token;
+    } catch (error) {
+      console.error('Error getting access token:', error.response ? error.response.data : error.message);
+      throw new Error('Failed to get access token');
+    }
   };
 
   const getCurrentlyPlayingTrack = async (accessToken) => {
-    const response = await axios.get('https://api.spotify.com/v1/me/player/currently-playing', {
-      headers: {
-        'Authorization': 'Bearer ' + accessToken,
-      },
-    });
-
-    return response.data;
+    try {
+      const response = await axios.get('https://api.spotify.com/v1/me/player/currently-playing', {
+        headers: {
+          'Authorization': 'Bearer ' + accessToken,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error('Error getting currently playing track:', error.response ? error.response.data : error.message);
+      throw new Error('Failed to get currently playing track');
+    }
   };
 
   try {
@@ -34,7 +42,7 @@ module.exports = async (req, res) => {
     const trackInfo = await getCurrentlyPlayingTrack(accessToken);
     res.status(200).json(trackInfo);
   } catch (error) {
-    console.error('Error fetching currently playing track:', error);
+    console.error('Server error:', error.message);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
